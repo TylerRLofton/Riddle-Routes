713e1d3b719b9e2a4310cd2b305ce10e75049226
JacksonSalyards <95992718+Thatthiccckid47@users.noreply.github.com>
2022-09-26 19:32:22 -0400

    Added a working map view

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+document.getElementById('zoom-out').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom - 1);
+};
+
+document.getElementById('zoom-in').onclick = function () {
+  const view = map.getView();
+  const zoom = view.getZoom();
+  view.setZoom(zoom + 1);
+};
