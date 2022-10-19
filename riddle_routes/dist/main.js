146269a98b5414422bd0ee7ce8e013bb1d9968d5
JacksonSalyards <95992718+Thatthiccckid47@users.noreply.github.com>
2022-10-19 16:34:31 -0400

    pushed the everything

---
+++ riddle_routes/dist/main.js
@@ -0,0 +1,68 @@
+import Feature from "ol/Feature";
+import Map from "ol/Map";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import View from "ol/View";
+import { Circle as CircleStyle, Fill, RegularShape, Stroke, Style } from "ol/style";
+import { LineString, Point } from "ol/geom";
+import { getVectorContext } from "ol/render";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { transform } from "ol/proj";
+import ZoomSlider from 'ol/control/ZoomSlider';
+import {defaults as defaultControls} from 'ol/control'
+
+var blueLightTowerCount = 4;
+const blueLightTower = new Array(blueLightTowerCount);
+var blueLightTowerPositionX = new Array(blueLightTowerCount);
+var blueLightTowerPositionY = new Array(blueLightTowerCount);
+
+for(let i = 0; i <= blueLightTowerCount; i++){
+    blueLightTowerPositionX[i] = 29.18818 + Math.random() * 0.0001 * i;
+    blueLightTowerPositionY[i] = -81.04965 - Math.random() * 0.0001 * i;
+}
+for (let i = 0; i < blueLightTowerCount; ++i) {
+  blueLightTower[i] = new Feature({
+    geometry: new Point(transform([blueLightTowerPositionY[i], blueLightTowerPositionX[i]], "EPSG:4326", "EPSG:3857")),
+    labelPoint: new Point(transform([blueLightTowerPositionY[i], blueLightTowerPositionX[i]], "EPSG:4326", "EPSG:3857")),
+    size: 15,
+    name: 'Blue Light Tower'+ i,
+  });
+};
+const BlueLightstyle = {"15": new Style({
+    image: new RegularShape({
+      points: 5,
+      radius: 7,
+      radius2: 3,
+      angle: 0,
+      fill: new Fill({ color: 'blue' }),
+      stroke: new Stroke({ color: 'black', width: 1 })
+    })
+  }),
+};
+const BlueLightvectorSource = new VectorSource({
+  features: blueLightTower,
+  wrapX: false
+});
+const BlueLightvector = new VectorLayer({
+  source: BlueLightvectorSource,
+  style: function (blueLightTower) {
+    return BlueLightstyle[blueLightTower.get("size")];
+  }
+});
+
+const mapView = new View({
+  center: transform([-81.04934, 29.18818], "EPSG:4326", "EPSG:3857"),
+  zoom: 18,
+  extent: [-9024000, 3398000,  -9021000, 3401000 ],
+});
+const map = new Map({
+  target: 'map',
+  layers: [new TileLayer({source: new OSM() }), BlueLightvector],
+  view: mapView,
+  controls: defaultControls().extend([new ZoomSlider()]),
+});
+document.getElementById('ToggleBlueLights').addEventListener('click', function () {
+ BlueLightvector.setVisible(!BlueLightvector.getVisible());
+});
+map.render();
